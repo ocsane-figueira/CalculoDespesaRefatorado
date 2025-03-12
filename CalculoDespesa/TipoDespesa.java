7d4b881720292a3cb8daff030d3faf134bca7d98
Ocsane Silva Figueira <153764244+ocsane-figueira@users.noreply.github.com>
2025-03-12 20:37:59 -0300

    TipoDespesa.java
    Comentário na classe para o max_value de carro

--- CalculoDespesa/TipoDespesa.java
+++ CalculoDespesa/TipoDespesa.java
@@ -8,6 +8,7 @@ public enum TipoDespesa {
     JANTAR("Jantar", 5000, true),
     CAFE_DA_MANHA("Café da Manhã", 1000, true),
     ALMOCO("Almoço", 2000, true),
+    //MAX_VALUE está atribuido ao Integer, ou seja, é o tamanho maximo que for atribuido nesse caso. Nao ha tratativa para ele pois nao ehComida
     ALUGUEL_CARRO("Aluguel de Carro", MAX_VALUE, false);
 
     private final String nome;
